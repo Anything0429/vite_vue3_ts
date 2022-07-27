309eea3b7a7e76376bd420c82a2023000a7eb7b4
郑建威 <453469696@qq.com>
2022-07-27 10:59:43 +0800

    commit 3

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,4 @@
-<!--
- * @Author: 郑建威 Zhengjw@zjcds.com
- * @Date: 2022-05-05 11:09:06
- * @LastEditors: 郑建威 Zhengjw@zjcds.com
- * @LastEditTime: 2022-05-05 11:22:43
- * @FilePath: \vue3-ts-practice\src\App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
-<!--
- * @Author: 郑建威 Zhengjw@zjcds.com
- * @Date: 2022-05-05 11:09:06
- * @LastEditors: 郑建威 Zhengjw@zjcds.com
- * @LastEditTime: 2022-05-05 11:17:09
- * @FilePath: \vue3-ts-practice\src\App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from '@/components/HelloWorld.vue'
 import {ref} from 'vue'
 
